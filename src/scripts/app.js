541f086187f63143e567054602a5d9dd74f8ee9e
ukarpenkov <truloveu@gmail.com>
2022-09-13 18:40:27 +0300

    add jquery

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,26 @@
-console.log('hella')
+import "./import-jquery";
+
+
+console.log('hella');
+// var newElems = $("<div class='dcell'>gggg</div>")
+//     .append("<img src='http://professorweb.ru/downloads/jquery/lily.png'/>")
+//     .append("<label for='lily'>Лилии:</label>")
+//     .append("<input name='lily' value='0' required />");
+// $('div').append(newElems)
+
+(function ($) {
+    $.fn.maxHeight = function () {
+
+        var max = 0;
+
+        this.each(function () {
+            max = Math.max(max, $(this).height());
+        });
+
+        return max;
+    };
+})(jQuery);
+
+var tallest = $('div').maxHeight()
+
+console.log(tallest)
