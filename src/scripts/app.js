75771e16de2c2efee5cdd1804f758b173cfd28a3
ukarpenkov <truloveu@gmail.com>
2022-09-14 18:28:42 +0300

    trying to make a simple plugin for experiment

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,25 +2,38 @@ import "./import-jquery";
 
 
 console.log('hella');
-// var newElems = $("<div class='dcell'>gggg</div>")
-//     .append("<img src='http://professorweb.ru/downloads/jquery/lily.png'/>")
-//     .append("<label for='lily'>Лилии:</label>")
-//     .append("<input name='lily' value='0' required />");
-// $('div').append(newElems)
 
 (function ($) {
-    $.fn.maxHeight = function () {
 
-        var max = 0;
-
-        this.each(function () {
-            max = Math.max(max, $(this).height());
-        });
-
-        return max;
+    var methods = {
+        init: function (options) {
+            return this
+        },
+        show: function () {
+            return this
+        },
+        changeColor: function (color) {
+            this.css({ 'background-color': color })
+        },
+        update: function (content) {
+            return this
+        }
     };
+
+    $.fn.tooltip = function (method) {
+
+        // логика вызова метода
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Метод с именем ' + method + ' не существует для jQuery.tooltip');
+        }
+    };
+
 })(jQuery);
 
-var tallest = $('div').maxHeight()
 
-console.log(tallest)
+$('div').tooltip('changeColor', 'blue');
+$('div').tooltip('update', 'Теперь тут новое содержимое');
