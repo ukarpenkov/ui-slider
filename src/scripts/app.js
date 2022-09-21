4684a0d53c61995a40adb9760a550053d174baab
ukarpenkov <truloveu@gmail.com>
2022-09-21 18:17:55 +0300

    experimenting with the slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,73 +3,111 @@ import "./import-jquery";
 
 console.log('hella');
 
-(function ($) {
+// (function ($) {
 
-    const methods = {
-        init: function () {
-            let $this = $(this)
-            let data = $this.data('newSlider')
-            let newSlider = $('<div>', {
-                'class': 'new-slider',
-                text: 'Новый текст сообщения.'
-            })
-            if (!data) {
-                // выполняем инит
-                $(this).data('newSlider', {
-                    target: $this,
-                    newSlider: newSlider
-                })
-                console.log('initialization is done!')
-            } else {
-                console.log('initialization is done before!')
-            }
-            return newSlider.appendTo(this)
-        },
-        show: function () {
-            return this
-        },
-        changeColor: function (color) {
-            return this.css({ 'background-color': color })
-        },
-        update: function () {
-            return this.css({ 'border': '6px solid' })
-        },
-        position: function (position) {
-            return this.css({
-                'position': position
-            })
-        },
-        destroy: function () {
-            return this.each(function () {
-                let $this = $(this)
-                let data = $this.data('newSlider')
-                $(window).unbind('.new-slider')
-                data.newSlider.remove()
-                $this.removeData('newSlider')
-                console.log('destroy is done')
-            })
+//     const methods = {
+//         init: function () {
+//             let $this = $(this)
+//             let data = $this.data('newSlider')
+//             let newSlider = $('<div>', {
+//                 'class': 'new-slider',
+//                 text: 'Новый текст сообщения.'
+//             })
+//             if (!data) {
+//                 // выполняем инит
+//                 $(this).data('newSlider', {
+//                     target: $this,
+//                     newSlider: newSlider
+//                 })
+//                 console.log('initialization is done!')
+//             } else {
+//                 console.log('initialization is done before!')
+//             }
+//             return newSlider.appendTo(this)
+//         },
+//         show: function () {
+//             return this
+//         },
+//         changeColor: function (color) {
+//             return this.css({ 'background-color': color })
+//         },
+//         update: function () {
+//             return this.css({ 'border': '6px solid' })
+//         },
+//         position: function (position) {
+//             return this.css({
+//                 'position': position
+//             })
+//         },
+//         destroy: function () {
+//             return this.each(function () {
+//                 let $this = $(this)
+//                 let data = $this.data('newSlider')
+//                 $(window).unbind('.new-slider')
+//                 data.newSlider.remove()
+//                 $this.removeData('newSlider')
+//                 console.log('destroy is done')
+//             })
+//         }
+//     };
+
+//     $.fn.ukslider = function (method) {
+
+
+//         if (methods[method]) {
+//             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+//         } else if (typeof method === 'object' || !method) {
+//             return methods.init.apply(this, arguments);
+//         } else {
+//             $.error('Метод с именем ' + method + ' не существует для jQuery.ukslider');
+//         }
+//     };
+
+// })(jQuery);
+
+
+
+// $('div').ukslider('init').ukslider('changeColor', 'yellow').ukslider('update')
+
+
+(function () {
+
+    function addSeparator(nStr) {
+        nStr += '';
+        var x = nStr.split('.');
+        var x1 = x[0];
+        var x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + '.' + '$2');
         }
-    };
+        return x1 + x2;
+    }
 
-    $.fn.ukslider = function (method) {
+    function rangeInputChangeEventHandler(e) {
+        console.log('gg')
+        var minBtn = $(this).parent().children('.min'),
+            maxBtn = $(this).parent().children('.max'),
+            range_min = $(this).parent().children('.range_min'),
+            range_max = $(this).parent().children('.range_max'),
+            minVal = parseInt($(minBtn).val()),
+            maxVal = parseInt($(maxBtn).val()),
+            origin = $(this).context.className;
 
-
-        if (methods[method]) {
-            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-        } else if (typeof method === 'object' || !method) {
-            return methods.init.apply(this, arguments);
-        } else {
-            $.error('Метод с именем ' + method + ' не существует для jQuery.ukslider');
+        if (origin === 'min' && minVal > maxVal - 5) {
+            $(minBtn).val(maxVal - 5);
         }
-    };
-
-})(jQuery);
-
+        var minVal = parseInt($(minBtn).val());
+        $(range_min).html(addSeparator(minVal * 1000) + ' €');
 
 
-$('.ukslider').ukslider('init').ukslider('changeColor', 'yellow').ukslider('update')
+        if (origin === 'max' && maxVal - 5 < minVal) {
 
-$('.ukslider').ukslider('destroy')
+            $(maxBtn).val(5 + minVal);
+        }
+        var maxVal = parseInt($(maxBtn).val());
+        $(range_max).html(addSeparator(maxVal * 1000) + ' €');
+    }
 
-$('.ukslider').ukslider('init').ukslider('changeColor', 'yellow').ukslider('update')
-
+    $('input').on('input', rangeInputChangeEventHandler);
+})();
