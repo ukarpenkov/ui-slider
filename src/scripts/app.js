1db405b2c1e5c37182129b1199093e52c22d6ca4
ukarpenkov <truloveu@gmail.com>
2022-11-03 16:07:43 +0300

    try to initial slider html with js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,6 @@
 import "./import-jquery";
 
 
-
 // (function ($) {
 
 //     const methods = {
@@ -69,44 +68,64 @@ import "./import-jquery";
 // $('div').ukslider('init').ukslider('changeColor', 'yellow').ukslider('update')
 
 
-(function handleRange() {
-    function rangeInputChangeEventHandler(e) {
-        var minBtn = $(this).parent().children('.js-uk-min');
-        var maxBtn = $(this).parent().children('.js-uk-max');
-        var range_min = $(this).parent().parent().children('.uk-slider__value_block').children('.js-uk-range_min');
-        var range_max = $(this).parent().parent().children('.uk-slider__value_block').children('.js-uk-range_max');
-        var minVal = Number($(minBtn).val());
-        var maxVal = Number($(maxBtn).val());
+function initSliders() {
+    let horizontalSlider = $('<div class="uk-slider"> <div class="uk-slider__range"> <input class="uk-slider__input uk-slider__input_handle_min js-uk-min" name="range_1" type="range" min="1" max="100" value="1" orient="vertical" /> <input class="uk-slider__input uk-slider__input_handle_max js-uk-max" name="range_1" type="range" min="1" max="100" value="100" orient="vertical" /> </div> </div> <div class="uk-slider__value_block"> <input type="text" value="1000" class="uk-slider__range_value uk-slider__range_value_min left js-uk-range_min" /> <input type="text" value="100000" class="uk-slider__range_value uk-slider__range_value_max right js-uk-range_max" /> </div>')
 
-        if (minVal > maxVal - 3) {
-            $(minBtn).val(maxVal - 3);
-        }
+    let verticalSlider = $('<div class="uk-slider"> <div class="uk-slider__range uk-slider__range_orient_vertical"> <input class="uk-slider__input uk-slider__input_handle_min js-uk-min" name="range_1" type="range" min="1" max="100" value="1" /> <input class="uk-slider__input uk-slider__input_handle_max js-uk-max" name="range_1" type="range" min="1" max="100" value="100" orient="vertical" /> </div> <div class="uk-slider__value_block uk-slider__value_block_orient_vertical" > <input type="text" value="1000" class="uk-slider__range_value uk-slider__range_value_min left js-uk-range_min" /> <input type="text" value="100000" class="uk-slider__range_value uk-slider__range_value_max right js-uk-range_max" /> </div> </div>')
 
-        $(range_min).change(function () {
-            $(minBtn).val($(range_min).val() / 1000)
-            if ($(range_min).val() > $(range_max).val() - 1000) {
+    return $('.id2').append(horizontalSlider).append(verticalSlider)
+
+
+
+}
+
+initSliders()
+
+
+
+
+
+    ((function handleRange() {
+        function rangeInputChangeEventHandler(e) {
+            var minBtn = $(this).parent().children('.js-uk-min');
+            var maxBtn = $(this).parent().children('.js-uk-max');
+            var range_min = $(this).parent().parent().children('.uk-slider__value_block').children('.js-uk-range_min');
+            var range_max = $(this).parent().parent().children('.uk-slider__value_block').children('.js-uk-range_max');
+            var minVal = Number($(minBtn).val());
+            var maxVal = Number($(maxBtn).val());
+
+            if (minVal > maxVal - 3) {
                 $(minBtn).val(maxVal - 3);
-                $(range_min).val($(range_max).val() - 1000)
             }
-        });
 
-        $(range_min).val((minVal * 1000));
+            $(range_min).change(function () {
+                $(minBtn).val($(range_min).val() / 1000)
+                if ($(range_min).val() > $(range_max).val() - 1000) {
+                    $(minBtn).val(maxVal - 3);
+                    $(range_min).val($(range_max).val() - 1000)
+                }
+            });
 
-        if (maxVal < minVal + 3) {
-            $(maxBtn).val(minVal + 3);
+            $(range_min).val((minVal * 1000));
+
+            if (maxVal < minVal + 3) {
+                $(maxBtn).val(minVal + 3);
+            }
+
+            $(range_max).change(function () {
+                $(maxBtn).val($(this).val() / 1000)
+                if ($(range_max).val() < $(range_min).val() - 1000) {
+                    $(maxBtn).val(maxVal + 3);
+                    $(range_max).val(Number($(range_min).val()) + 1000)
+                }
+            });
+            $(range_max).val((maxVal * 1000));
         }
 
-        $(range_max).change(function () {
-            $(maxBtn).val($(this).val() / 1000)
-            if ($(range_max).val() < $(range_min).val() - 1000) {
-                $(maxBtn).val(maxVal + 3);
-                $(range_max).val(Number($(range_min).val()) + 1000)
-            }
-        });
-        $(range_max).val((maxVal * 1000));
-    }
+        $('.uk-slider__input').on('input', rangeInputChangeEventHandler);
+        $('.uk-slider__input').trigger('input');
 
-    $('.uk-slider__input').on('input', rangeInputChangeEventHandler);
-    $('.uk-slider__input').trigger('input');
+    }))()
 
-})()
+
+
