27475d7dec98b6a8f4c45a56664e5e4e373e025f
ukarpenkov <truloveu@gmail.com>
2022-11-09 17:46:47 +0300

    min max values without x1000

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,9 +10,9 @@ class SliderSettings {
     }
 }
 
-let settings1 = new SliderSettings('interval', 'horizontal', 1, 100)
-let settings2 = new SliderSettings('interval', 'vertical', 1, 100)
-let settings3 = new SliderSettings('single', 'horizontal', 1, 100)
+let settings1 = new SliderSettings('interval', 'horizontal', 1, 200)
+let settings2 = new SliderSettings('interval', 'vertical', 1, 300)
+let settings3 = new SliderSettings('single', 'horizontal', 1, 300)
 
 initSlider(".id2", settings1)
 initSlider(".id3", settings2)
@@ -35,8 +35,8 @@ function initSlider(wrapper, settings) {
     let horizontalSlider = $(`<div class="uk-slider__range ${orientation}"> <input class="uk-slider__input uk-slider__input_handle_min js-uk-min" name="range_1" type="range" min="${settings.minValue}" max="${settings.maxValue}" value="1" orient="vertical" /> <input class="uk-slider__input uk-slider__input_handle_max js-uk-max ${visible}" name="range_1" type="range" min="${settings.minValue}" max="${settings.maxValue}" value="100" orient="vertical" /> </div> </div> <div class="uk-slider__value_block ${inputsOrientation}"> <input type="text" value="${settings.minValue}" class="uk-slider__range_value uk-slider__range_value_min left js-uk-range_min" /> <input type="text" value="${settings.maxValue}" class="uk-slider__range_value uk-slider__range_value_max right js-uk-range_max ${visible}" />`)
 
     $(wrapper).append(horizontalSlider)
-
 }
+
 
 (function handleRange() {
     function rangeInputChangeEventHandler(e) {
@@ -46,40 +46,29 @@ function initSlider(wrapper, settings) {
         var range_max = $(this).parent().parent().children('.uk-slider__value_block').children('.js-uk-range_max');
         var minVal = Number($(minBtn).val());
         var maxVal = Number($(maxBtn).val());
-
         if (minVal > maxVal - 3) {
             $(minBtn).val(maxVal - 3);
         }
-
         $(range_min).change(function () {
-            $(minBtn).val($(range_min).val() / 1000)
-            if ($(range_min).val() > $(range_max).val() - 1000) {
+            $(minBtn).val($(this).val() / 1)
+            if ($(range_min).val() > $(range_max).val() - 1) {
                 $(minBtn).val(maxVal - 3);
-                $(range_min).val($(range_max).val())
+                $(range_min).val($(range_max).val() - 1)
             }
         });
-
-        $(range_min).val((minVal * 1000));
-
+        $(range_min).val((minVal * 1));
         if (maxVal < minVal + 3) {
             $(maxBtn).val(minVal + 3);
         }
-
         $(range_max).change(function () {
-            $(maxBtn).val($(this).val() / 1000)
-            if ($(range_max).val() < $(range_min).val() - 1000) {
+            $(maxBtn).val($(this).val() / 1)
+            if ($(range_max).val() < $(range_min).val() - 1) {
                 $(maxBtn).val(maxVal + 3);
-                $(range_max).val(Number($(range_min).val()) + 1000)
+                $(range_max).val(Number($(range_min).val()) + 1)
             }
         });
-        $(range_max).val((maxVal * 1000));
+        $(range_max).val((maxVal * 1));
     }
-
     $('.uk-slider__input').on('input', rangeInputChangeEventHandler)
-
     $('.uk-slider__input').trigger('input')
-
 })()
-
-
-
