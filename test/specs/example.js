0ab96139ba4d375ac82c4a620bebba9cde8cb3e1
ukarpenkov <truloveu@gmail.com>
2023-01-17 19:48:27 +0300

    testing

--- test/specs/example.js
+++ test/specs/example.js
@@ -1,4 +1,5 @@
 
+
 describe('Test case', () => {
     it('site title is "ui-slider"', async () => {
         await browser.url('http://localhost:1234/')
@@ -6,9 +7,19 @@ describe('Test case', () => {
     })
     it('change min pos to 10', async () => {
         await browser.url('http://localhost:1234/')
-        const wrapper = $('.id2')
-        await wrapper.$('.js-uk-range_min').setValue(10)
-        await browser.pause(10000)
+        const wrapper = await $('.id2')
+        const body = await $('body')
+        const minValueSliderInput = await wrapper.$('.js-uk-range_min')
+        const minBtn = await wrapper.$('.js-uk-min')
+        await minValueSliderInput.setValue(10)
+        // await browser.pause(3000)
+        await body.click()
+        await browser.pause(3000)
 
+
+        const minBtnValue = await minBtn.getValue()
+        const minValueSliderInputVal = await minValueSliderInput.getValue()
+        console.log('оооооооооооооооОООООООООООООООООООООООООООООООПППП', minBtnValue == minValueSliderInputVal)
+        ExpectedConditions.numberOfElementsToBe(minBtnValue, 100)
     })
 })
