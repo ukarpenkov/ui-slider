e7607f16440c2c9f2a835a5cba1c5f5a840e2cc8
ukarpenkov <truloveu@gmail.com>
2024-03-12 23:00:37 +0300

    try to '--no-minify' build

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,6 @@
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
-  return (mod && mod.__esModule) ? mod : { "default": mod };
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
 require("./utils/import-jquery");
