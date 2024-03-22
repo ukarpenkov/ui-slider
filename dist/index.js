ca943cc0baff41615ad47c01b755c2cbee8f9869
ukarpenkov <truloveu@gmail.com>
2024-03-22 23:42:27 +0300

    --no-optimize build

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+import { $67aff7fbf5506288$export$2e2bcd8739ae039, $b29e4f8572f5178c$export$2b57d60599a63da5, $b29e4f8572f5178c$export$92d4e20e0acb0b06, $ea895fed1e902031$export$f1e8abe023402588 } from "./index.20a44db0";
+
+let $859d9b41ce0aaed4$var$slider1 = new (0, $ea895fed1e902031$export$f1e8abe023402588)("1", "interval", "horizontal", 1, 50, 1, 100, 1, true, true);
+$859d9b41ce0aaed4$var$slider1.init();
+let $859d9b41ce0aaed4$var$slider2 = new (0, $ea895fed1e902031$export$f1e8abe023402588)("2", "interval", "vertical", 1, 20, 1, 20, 1, true, true);
+$859d9b41ce0aaed4$var$slider2.init();
+let $859d9b41ce0aaed4$var$slider3 = new (0, $ea895fed1e902031$export$f1e8abe023402588)("3", "single", "horizontal", 1, 20, 1, 20, 1, true, true);
+$859d9b41ce0aaed4$var$slider3.init();
+(0, $67aff7fbf5506288$export$2e2bcd8739ae039)(".slider-page");
+(0, $b29e4f8572f5178c$export$2b57d60599a63da5)();
+(0, $b29e4f8572f5178c$export$92d4e20e0acb0b06)();
